d3ef29f8aa34231599bda79235eafa1c94f586d1
Kenya Chan <kenya.chan@gmail.com>
2022-08-21 00:21:26 +1000

    Add comments to explain logic

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,9 @@
 const leapYears = function(year) {
+
+    // if a year is divisible by 4, it's a leap year
     let isLeapYear = year % 4 === 0 ? true : false;
 
+    // however, if it is divisible by and not divisible by 400, it's not a leap year
     if (year % 100 === 0 && year % 400 !== 0) isLeapYear = false;
 
     return isLeapYear;
