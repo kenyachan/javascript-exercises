f71acb3265e312f94146db062da135378c3721b2
Kenya Chan <kenya.chan@gmail.com>
2022-08-20 21:40:08 +1000

    Complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, repetition) {
+    let repeatedString = "";
 
+    if (repetition < 0) return 'ERROR';
+
+    for (let i = 0; i < repetition; i++) {
+        repeatedString = `${repeatedString}${string}`;
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line
