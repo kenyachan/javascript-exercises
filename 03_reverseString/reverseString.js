a4feb485cf60ec823527fecbb57f8785f6215561
Kenya Chan <kenya.chan@gmail.com>
2022-08-20 22:22:19 +1000

    Complete exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = "";
+    let stringArray = string.split("");
 
+    for (let i = stringArray.length; i > 0; i--) {
+        reversedString = `${reversedString}${stringArray[i - 1]}`
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
