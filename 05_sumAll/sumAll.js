57071421bbe314450863f4533aaf6d0411e55a2f
Kenya Chan <kenya.chan@gmail.com>
2022-08-20 23:58:49 +1000

    Complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(fromNumber, toNumber) {
+    let total = 0;
 
+    if (fromNumber < 0 || toNumber < 0) return "ERROR";
+    if (typeof fromNumber !== "number" || typeof toNumber !== "number") return "ERROR";
+
+    let from = fromNumber < toNumber ? fromNumber : toNumber;
+    let to = fromNumber > toNumber ? fromNumber : toNumber;
+
+    for (let i = from; i <= to; i++) {
+        total = total + i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
