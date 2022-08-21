24a20dc2b1ac9c9b7be6fbdb869e8b23a78f8c5d
Kenya Chan <kenya.chan@gmail.com>
2022-08-21 16:11:45 +1000

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest;
 
+    people.forEach(person => {
+        if (oldest === undefined) {
+            oldest = person;
+        } else if (getAge(person) > getAge(oldest)) {
+            oldest = person;
+        }
+    });
+
+    return oldest;
 };
+
+function getAge(person) {
+    if (person.yearOfDeath) {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+
+    return age = new Date().getFullYear() - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
