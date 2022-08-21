b5da4d32e7afd3c493e4a0028ffd35a9522a6081
Kenya Chan <kenya.chan@gmail.com>
2022-08-21 15:01:25 +1000

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+let reverseString = require('../03_reverseString/reverseString');
 
+const palindromes = function (string) {
+    let cleanString = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    
+    return reverseString(cleanString) === cleanString ? true : false;
 };
 
 // Do not edit below this line
