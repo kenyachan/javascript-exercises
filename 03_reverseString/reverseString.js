67ba0a1657af57d458e8c0b7ecdd6b8498e64d16
Kenya Chan <kenya.chan@gmail.com>
2022-08-21 16:30:24 +1000

    Update answer for efficiency

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,12 +1,14 @@
 const reverseString = function(string) {
-    let reversedString = "";
-    let stringArray = string.split("");
+    // let reversedString = "";
+    // let stringArray = string.split("");
 
-    for (let i = stringArray.length; i > 0; i--) {
-        reversedString = `${reversedString}${stringArray[i - 1]}`
-    }
+    // for (let i = stringArray.length; i > 0; i--) {
+    //     reversedString = `${reversedString}${stringArray[i - 1]}`
+    // }
 
-    return reversedString;
+    // return reversedString;
+
+    return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
